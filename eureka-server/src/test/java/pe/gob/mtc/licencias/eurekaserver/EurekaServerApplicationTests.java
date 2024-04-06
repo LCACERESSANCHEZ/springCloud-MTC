ce2fe55b72eebbdaf0e326d68d49881cd91a6d13
lcaceres <luisfernandocaceressanchez@gmail.com>
2024-04-06 17:09:34 -0500

    Day 1 arquitecturaSolucion and day 2 EurekaServer and ConfigServer

---
+++ eureka-server/src/test/java/pe/gob/mtc/licencias/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.gob.mtc.licencias.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
